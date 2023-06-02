5b5f84761ebe59e8b808e7b3fcbc5b3d6d129130
Rhenald <renaldkarel@gmail.com>
2023-06-02 21:11:53 +0700

    feat: badge/tag/label component

---
+++ src/components/ui/badge/index.jsx
@@ -0,0 +1,7 @@
+export default function Badge({ children }) {
+	return (
+		<span className='bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300'>
+			{children}
+		</span>
+	);
+}
