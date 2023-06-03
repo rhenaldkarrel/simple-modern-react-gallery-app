1f093c1576b54ab6d378600c9cdbc8eda05dc6db
Rhenald <renaldkarel@gmail.com>
2023-06-03 09:59:03 +0700

    add active state on category link filter

--- src/components/category-link/index.jsx
+++ src/components/category-link/index.jsx
@@ -1,13 +1,17 @@
 import clsx from 'clsx';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 export default function CategoryLink({ children }) {
+	const [searchParams] = useSearchParams();
+	const categoryParam = searchParams.get('category');
+
 	return (
 		<li className='nav-item'>
 			<Link
 				className={clsx(
-					'px-12 uppercase transition-all py-2',
-					'hover:border-b hover:border-b-black'
+					'px-12 uppercase transition-all py-2 whitespace-nowrap',
+					'hover:border-b hover:border-b-black',
+					{ 'border-b border-b-black': categoryParam === children }
 				)}
 				to={`?category=${children}`}
 			>
