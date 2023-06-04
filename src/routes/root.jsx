444112886ae5718b7782074c50fc13e58b34f63c
Rhenald <renaldkarel@gmail.com>
2023-06-04 16:08:39 +0700

    feat: add lazy loading pages

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,11 +1,33 @@
-import { createBrowserRouter } from 'react-router-dom';
-import { Gallery, Home } from '../pages';
+import { Navigate, Outlet, createBrowserRouter } from 'react-router-dom';
+import React from 'react';
+
 import AppLayout from '../components/layout/app-layout';
+import { lazyImport } from '../lib/utils';
+import { Spinner } from '../components/ui/spinner';
+
+const { Home } = lazyImport(() => import('../pages'), 'Home');
+const { Gallery } = lazyImport(() => import('../pages'), 'Gallery');
+
+const App = () => {
+	return (
+		<React.Suspense
+			fallback={
+				<div className='h-screen w-full flex items-center justify-center'>
+					<Spinner size='xl' />
+				</div>
+			}
+		>
+			<AppLayout>
+				<Outlet />
+			</AppLayout>
+		</React.Suspense>
+	);
+};
 
 const router = createBrowserRouter([
 	{
 		path: '/',
-		element: <AppLayout />,
+		element: <App />,
 		children: [
 			{
 				index: true,
@@ -15,6 +37,7 @@ const router = createBrowserRouter([
 				path: '/gallery',
 				element: <Gallery />,
 			},
+			{ path: '*', element: <Navigate to='.' /> },
 		],
 	},
 ]);
